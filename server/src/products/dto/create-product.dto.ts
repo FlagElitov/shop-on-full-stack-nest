541a6d03563e63cccfcdb33737815996b27f2c1c
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2020-12-06 20:07:28 +0200

    upgrade

--- server/src/products/dto/create-product.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateProductDto {
-  readonly title: string;
-  readonly price: number;
-  readonly company: string;
-  readonly image: string;
-  readonly rating: number;
-}
--- server/src/products/dto/update-product.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UpdateProductDto {
-  readonly title: string;
-  readonly price: number;
-  readonly company: string;
-  readonly image: string;
-  readonly rating: number;
-}
